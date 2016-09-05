70fc608a8791acdb6331a021eeb388303438141b
나즈나 <nazuna@nazuna.kr>
2016-09-05 10:18:04 +0900

    샘플2 최종

---
+++ Sample2/src/HelloFriend2.java
@@ -0,0 +1,11 @@
+
+public class HelloFriend2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 2030;
+		System.out.println("헬로"); // 자주쓰는 참조 자동입력 Ctrl + Space
+		
+	}
+
+}
