d1818c18d682135bad324f76b9d924cc793928be
나즈나 <nazuna@nazuna.kr>
2016-09-05 11:13:44 +0900

    직사각형 둘레 면적 구하기

---
+++ chap2/src/ex2_2_rect/Box.java
@@ -0,0 +1,20 @@
+package ex2_2_rect;
+
+public class Box {
+
+	public static void main(String[] args) {
+		double w;
+		double h;
+		double area;
+		double perimeter;
+		w = 10.0;
+		h = 5.0;
+		area = w*h;
+		perimeter = 2.0*(w+h);
+		System.out.println("사각형의 넓이: " + area);
+		System.out.println("사각형의 둘레: " + perimeter);
+
+
+	}
+
+}
