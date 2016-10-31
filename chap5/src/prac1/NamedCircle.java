00c882465657433882e4fb0f53cf9b0b0b3aff9f
나즈나 <nazuna@nazuna.kr>
2016-10-31 10:36:28 +0900

    챕터5 실습1 추가

---
+++ chap5/src/prac1/NamedCircle.java
@@ -0,0 +1,31 @@
+package prac1;
+
+class Circle {
+	private int radius;
+	public Circle(int radius){
+		this.radius = radius;
+	}
+	public int getRadius() {
+		return radius;
+	}
+}
+
+public class NamedCircle extends Circle {
+	private String name;
+	NamedCircle(int radius, String name) {
+		super(radius);
+		this.name = name;
+	}
+	
+	void show() {
+		System.out.println(name + ", ¹ÝÁö¸§ = " + getRadius());
+	}
+	
+
+	public static void main(String[] args) {
+		NamedCircle w = new NamedCircle(5, "Waffle");
+		w.show();
+
+	}
+
+}
