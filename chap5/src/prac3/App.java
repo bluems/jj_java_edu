4fae08d64ab2e72396a4bc1e31ae16a0ea7ea3a3
nazuna1 <nazuna@nazuna.kr>
2016-11-07 11:09:09 +0900

    실습 3 추가

---
+++ chap5/src/prac3/App.java
@@ -0,0 +1,44 @@
+package prac3;
+
+import java.util.Scanner;
+
+abstract class Calculator {
+	protected int a, b;
+	abstract protected int calc();
+	protected void input() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("정수 두개를 입력하세요 >> ");
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+	}
+	
+	public void run() {
+		input();
+		int res = calc();
+		System.out.println("계산된 값은 " + res);
+	}
+}
+
+class Adder extends Calculator {
+	protected int calc() {
+		return a + b;
+	}
+}
+
+class Substracter extends Calculator {
+	protected int calc() {
+		return a - b;
+	}
+}
+
+public class App {
+
+	public static void main(String[] args) {
+		Adder adder = new Adder();
+		Substracter sub = new Substracter();
+		
+		adder.run();
+		sub.run();
+	}
+
+}
