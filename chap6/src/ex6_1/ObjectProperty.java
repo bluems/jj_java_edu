aff574091727b9bd85dcc06a3e21caac59a44312
나즈나 <nazuna@nazuna.kr>
2016-11-14 11:34:49 +0900

    예제 6-1 추가

---
+++ chap6/src/ex6_1/ObjectProperty.java
@@ -0,0 +1,15 @@
+package ex6_1;
+
+public class ObjectProperty {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Point p = new Point(2,3);
+		
+		System.out.println(p.getClass().getName());
+		System.out.println(p.getClass());
+		System.out.println(p.hashCode());
+		System.out.println(p.toString());
+	}
+
+}
---
+++ chap6/src/ex6_1/Point.java
@@ -0,0 +1,9 @@
+package ex6_1;
+
+class Point {
+	int x, y;
+	
+	public Point(int x, int y) {
+		this.x = x; this.y = y;
+	}
+}
