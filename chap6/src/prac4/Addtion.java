9ea4734e2e86620832936eea41e542a8772a053a
나즈나 <nazuna@nazuna.kr>
2016-11-14 11:43:12 +0900

    실습 4 중간 추가

---
+++ chap6/src/prac4/Addtion.java
@@ -0,0 +1,22 @@
+package prac4;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Addtion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		String exp = scanner.nextLine();
+		StringTokenizer st = new StringTokenizer(exp, "+");
+		int sum = 0;
+		
+		while(st.hasMoreTokens()) {
+			
+		}
+		
+		System.out.println("гую╨ " + sum);
+	}
+
+}
