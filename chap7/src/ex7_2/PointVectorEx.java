4628a64cb0f357e43a27767d292385ccd729958a
나즈나 <nazuna@nazuna.kr>
2016-11-21 10:18:29 +0900

    예제 7-2 추가

---
+++ chap7/src/ex7_2/PointVectorEx.java
@@ -0,0 +1,24 @@
+package ex7_2;
+
+import java.util.Vector;
+
+public class PointVectorEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vector<Point> v = new Vector<Point>();
+		
+		v.add(new Point(2,3));
+		v.add(new Point(-5,20));
+		v.add(new Point(30, -8));
+		
+		v.remove(1);
+		
+		for(int i = 0 ; i<v.size() ; i++) {
+			Point p = v.get(i);
+			System.out.println(p);
+		}
+
+	}
+
+}
