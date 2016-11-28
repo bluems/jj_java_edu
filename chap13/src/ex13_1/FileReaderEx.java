60ed2c97ae97082288c38f485bc625995ae4f843
나즈나 <nazuna@nazuna.kr>
2016-11-28 11:13:40 +0900

    예제 13-1 수정

--- chap13/src/ex13_1/FileReaderEx.java
+++ chap13/src/ex13_1/FileReaderEx.java
@@ -6,11 +6,17 @@ public class FileReaderEx {
 
 	public static void main(String[] args) throws IOException{
 		FileReader in = new FileReader("e:\\test.txt");
-		int c;
+		int readCharNo;
+		char[] cbuf = new char[5];
+		String data = "";
 			
-		while((c = in.read()) != -1) {
-			System.out.print((char)c);
+		while(true){
+			readCharNo = in.read(cbuf, 1, 3);
+			if (readCharNo == -1) break;
+			data += new String(cbuf, 0, readCharNo);
 		}
+		
+		System.out.print(data);
 		in.close();
 	}
 
