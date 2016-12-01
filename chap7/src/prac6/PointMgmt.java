b5fe26a5b0d814ec26092b9f0f6279516e9c384d
나즈나 <nazuna@nazuna.kr>
2016-12-02 00:58:45 +0900

    16.12.02 실습 7-6 기능 최종

--- chap7/src/prac6/PointMgmt.java
+++ chap7/src/prac6/PointMgmt.java
@@ -1,31 +1,28 @@
 package prac6;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Scanner;
 
 public class PointMgmt {
-
+	static Scanner scanner = new Scanner(System.in);
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
-		HashMap<String, MemberInfo> MemberList = new HashMap<String,MemberInfo>();
-		Scanner scanner = new Scanner(System.in);
+		
 		
 		MainLoop:while(true) {
-			System.out.println("1. 고객 등록 2. 고객 관리 3. 포인트 조회");
+			System.out.println("1. 등록 2. 조회 3. 관리");
 			if (scanner.hasNextInt())
 				switch (scanner.nextInt()) {
 					
 					case 1:
-						Add(MemberList);
+						Management.Add();
 						break;
 					case 2:
-						
+						MainCaseInQuery();
 						break;
 					case 3:
-						
+						Management manage = new Management();
+						manage.start();
 						break;
 					case 0:
 					default:
@@ -36,36 +33,20 @@ public class PointMgmt {
 		scanner.close();
 	}
 	
-	public static void Add(HashMap<String, MemberInfo> MemberList) {
-		Scanner scanner = new Scanner (System.in);
+	public static void MainCaseInQuery() {
 		
-		System.out.print("(이름) > ");
-		String name = scanner.nextLine();
 		
-		System.out.print("(나이) > ");
-		//byte age = scanner.nextByte();
-		String s_age = scanner.next();
-		int age = Integer.parseInt(s_age);
-		
-		//scanner.skip("[\\r\\n]+");
-		
-		System.out.println("(주소) > ");
-		scanner.nextLine();
-		String addr = scanner.nextLine();
-		
-		MemberList.put(name, new MemberInfo(age, addr, 0));
-	}
-	
-	public static void View(HashMap<String,MemberInfo> MemberList) {
-		Iterator<String> it = MemberList.keySet().iterator();
-		
-		while(it.hasNext()) {
-			String name = (String)it.next();
-			MemberInfo info = MemberList.get(name);
-			System.out.println("고객명 : " + name + "나이 : " + info.age + ", 포인트 : " + info.point + ", 주소 : " + info.addr);
+		InQueryLoop:while(true) {
+			System.out.println("1. 전체 명단 0.이전 메뉴");
 			
+			if (scanner.hasNextInt())
+				switch(scanner.nextInt()) {
+				case 1:
+					Management.View();
+				case 0:
+					break InQueryLoop;
+				}
 		}
-		
 	}
 
 }
